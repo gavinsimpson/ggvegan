3c80fc7a6718499c7be7a47e536b66b24b546239
Gavin Simpson <ucfagls@gmail.com>
2013-04-25 08:54:11 -0600

    used wrong dimlabel for x axis

--- R/autoplot.metaMDS.R
+++ R/autoplot.metaMDS.R
@@ -55,7 +55,7 @@
                                aes(x = Dim1, y = Dim2, colour = Score))
     }
     if(missing(xlab))
-        xlab <- dimlabels[2]
+        xlab <- dimlabels[1]
     if(missing(ylab))
         ylab <- dimlabels[2]
     plt <- plt + xlab(xlab) + ylab(ylab)
