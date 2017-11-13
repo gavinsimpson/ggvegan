7901b05a057e3a7f551af71c60cc7bb320a9fb6b
Gavin Simpson <ucfagls@gmail.com>
2017-11-12 18:34:11 -0600

    remove the superfluous utils stuff now that we're using aes_string

--- R/autoplot.cca.R
+++ R/autoplot.cca.R
@@ -1,13 +1,3 @@
-## ## help codetools "find" variables - i.e. ignore the warnings in the
-## ## ggplot() calls below - doing this on per-function basis in case
-## ## R goes that way in the future. As ?globalVariables states, this
-## ## is really active at the package level
-## if(getRversion() >= "2.15.1") {
-##     utils::globalVariables(c("Score", "Label"))
-## }
-## ## end help codetools
-
-
 ##' @title ggplot-based plot for objects of class \code{"cca"}
 ##'
 ##' @description
