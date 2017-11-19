7ec3c73b6535173581fb01ce03b1128b199327d7
Gavin Simpson <ucfagls@gmail.com>
2017-11-18 18:19:51 -0600

    add show.fitted and title arguments to fisherfit autoplot method

--- R/autoplot.fisherfit.R
+++ R/autoplot.fisherfit.R
@@ -3,8 +3,10 @@
 ##' @description Draws a bar plot of species rank abundance with Fisher's log-series superimposed.
 ##'
 ##' @param object an object of class \code{\link{fisherfit}}.
+##' @param show.fitted logical; should the estimated distribution also be plotted?
 ##' @param xlab character; label for the x axis.
 ##' @param ylab character; label for the y axis.
+##' @param title character; title for the plot.
 ##' @param bar.col colour for the bar outlines. The default, \code{NA}, does not draw outlines around bars.
 ##' @param bar.fill fill colour for the bars.
 ##' @param line.col colour for Fisher's log-series curve.
@@ -22,7 +24,9 @@
 ##' data(BCI)
 ##' mod <- fisherfit(BCI[5,])
 ##' autoplot(mod)
-`autoplot.fisherfit` <- function(object, xlab = "Rank", ylab = "Abundance",
+`autoplot.fisherfit` <- function(object, show.fitted = TRUE,
+                                 xlab = "Abundance", ylab = "Number of Species",
+                                 title = "Fisher's log-series distribution",
                                  bar.col = NA, bar.fill = "grey35",
                                  line.col = "red", size = 1,
                                  ...) {
@@ -32,10 +36,12 @@
     df <- fortify(object)
     plt <- ggplot(df, aes_string(x = 'Rank', y = 'Abundance')) +
         geom_bar(stat = 'identity', colour = bar.col, fill = bar.fill)
-    alpha <- object[['estimate']]
-    k <- object[['nuisance']]
-    plt <- plt + stat_function(fun = fishfun, args = list(k = k, alpha = alpha),
-                               colour = line.col, size = size) +
-        labs(x = xlab, y = ylab)
+    if (show.fitted) {
+        alpha <- object[['estimate']]
+        k <- object[['nuisance']]
+        plt <- plt + stat_function(fun = fishfun, args = list(k = k, alpha = alpha),
+                                   colour = line.col, size = size)
+    }
+    plt <- plt + labs(x = xlab, y = ylab, title = title)
     plt
 }
