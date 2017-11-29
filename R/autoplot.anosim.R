7a6f9a93bf1cc369326ec48df1d68131cc4a5279
Didzis <didzis.elferts@lu.lv>
2017-11-29 19:11:18 +0200

    Changed code according to reviewer suggestions.

--- R/autoplot.anosim.R
+++ R/autoplot.anosim.R
@@ -8,35 +8,46 @@
 ##' @param varwidth logical; make box width proportional to to the square-roots of the number of observations in the groups (default) or standart box plot?
 ##' @param xlab character; label for the x-axis.
 ##' @param ylab character; label for the y-axis.
-##' @param subtitle logical; should subtitle with R and p values be added.
+##' @param title character; title for the plot
+##' @param subtitle character; subtitle for the plot
+##' @param caption character; caption for the plot
+##' @param show.stats logical; should R and p values be added to the plot.
 ##' @param ... additional arguments passed to other methods.
 ##' @return A ggplot object.
 ##' @author Didzis Elferts
 ##'
 ##' @export
 ##'
-##' @importFrom ggplot2 ggplot autoplot geom_boxplot aes_string labs fortify
+##' @importFrom ggplot2 ggplot autoplot geom_boxplot aes_string labs fortify annotate
 ##'
 ##' @examples
 ##'
 ##' data(dune)
 ##' data(dune.env)
 ##' dune.dist <- vegdist(dune)
-##' attach(dune.env)
-##' dune.ano <- anosim(dune.dist, Management)
+##' dune.ano <- with(dune.env, anosim(dune.dist, Management))
 ##'
 ##' autoplot(dune.ano)
 
-`autoplot.anosim` <- function(object, notch = TRUE, varwidth = TRUE, subtitle = TRUE,
-                              xlab = NULL, ylab = NULL, ...) {
-  ranks <- fortify(object, what = "ranks")
-  df.title <- fortify(object, what = "stats")
+`autoplot.anosim` <- function(object, notch = TRUE, varwidth = TRUE, show.stats = TRUE,
+                              xlab = NULL, ylab = NULL, title = "Results of analysis of similarities",
+                              caption = NULL, subtitle = NULL, ...) {
+  df <- fortify(object)
   
-  plt <- ggplot(ranks, aes_string(x = "Class", y = "Rank")) +
+  plt <- ggplot(df, aes_string(x = "Class", y = "Rank")) +
     geom_boxplot(notch = notch, varwidth = varwidth) + 
-    labs(x = xlab, y = ylab)
-  if(subtitle == TRUE) {
-    plt = plt + labs(subtitle = paste("R = ", df.title$Value[2], ", ", "P = ", df.title$Value[1]))
+    labs(x = xlab, y = ylab, title = title, caption = caption)
+  
+  if(isTRUE(show.stats)) {
+    if (object$permutations) {
+      pval <- format.pval(object$signif)
+    }
+    else {
+      pval <- "not assessed"
+    }
+    rval <- round(object$statistic,3)
+    plt <- plt + annotate("text", x = 0.5, y = max(df$Rank, na.rm = TRUE) *1.05, hjust = 0, vjust = 0,
+                          label = paste("R = ", rval, ", ", "P = ", pval))
   }
   plt
 }
--- R/fortify.anosim.R
+++ R/fortify.anosim.R
@@ -1,11 +1,9 @@
 ##' @title Fortify analysis of similarities (ANOSIM) results
 ##' @description Prepares fortified versions of results from \code{\link[vegan]{anosim}} objects.
-##' @details TODO
 ##' @param model an object of class \code{\link[vegan]{anosim}}.
 ##' @param data original data set. Currently ignored.
-##' @param what character; what aspect of the results should be fortified? \code{"ranks"} returns ranks of dissimilarity entries and names of classies. \code{"stats"} returns significance from permutation  and the value of ANOSIM statistic R.
 ##' @param ... additional arguments for other methods. Currently ignored.
-##' @return A data frame. For \code{what = "ranks"}, the data frame contains columns \code{'Rank'} and \code{'Class'} with ranks of dissimilarity. For \code{what = "stats"}, the data frame contains columns \code{'Parameter'} and \code{'Value'} with values permutation test and R statistic.
+##' @return A data frame with columns \code{'Rank'} and \code{'Class'} with ranks of dissimilarity.
 ##'
 ##' @export
 ##'
@@ -17,33 +15,17 @@
 ##' data(dune)
 ##' data(dune.env)
 ##' dune.dist <- vegdist(dune)
-##' attach(dune.env)
-##' dune.ano <- anosim(dune.dist, Management)
+##' dune.ano <- with(dune.env, anosim(dune.dist, Management))
 ##' 
-##' df <- fortify(dune.ano, what = "ranks")
-##' df.title <- fortify(dune.ano, what = "stats")
+##' df <- fortify(dune.ano)
 ##' 
 ##' ggplot(df, aes(x = Class, y = Rank)) +
 ##'       geom_boxplot(notch = TRUE, varwidth = TRUE) + 
-##'       labs(subtitle = paste("R = ", df.title$Value[2], ", ", "P = ", df.title$Value[1])) +
 ##'       theme(axis.title = element_blank())
-`fortify.anosim` <- function(model, data, 
-                             what = c("ranks","stats"),...) {
+##'       
+`fortify.anosim` <- function(model, data, ...) {
   
-  what <- match.arg(what)
-  
-  if (isTRUE(all.equal(what, "ranks"))) {
-      df <- data.frame(Rank = model$dis.rank,
+  df <- data.frame(Rank = model$dis.rank,
                    Class = model$class.vec)
-  } else {
-    if (model$permutations) {
-      pval <- format.pval(model$signif)
-    }
-    else {
-      pval <- "not assessed"
-    }
-    df <- data.frame(Parameter = c("P", "R"),
-                     Value = c(pval, round(model$statistic,3)))
-  }
   df
 }
