f05ad82e3936c7caa4d0a17a122e6c44f9f5b255
Gavin L. Simpson <ucfagls@gmail.com>
2017-12-04 11:49:30 -0600

    use [[ for subsetting; tweak example

--- R/fortify.anosim.R
+++ R/fortify.anosim.R
@@ -1,9 +1,9 @@
 ##' @title Fortify analysis of similarities (ANOSIM) results
-##' @description Prepares fortified versions of results from \code{\link[vegan]{anosim}} objects.
+##' @description Prepares a fortified version of results from \code{\link[vegan]{anosim}} objects.
 ##' @param model an object of class \code{\link[vegan]{anosim}}.
 ##' @param data original data set. Currently ignored.
 ##' @param ... additional arguments for other methods. Currently ignored.
-##' @return A data frame with columns \code{'Rank'} and \code{'Class'} with ranks of dissimilarity.
+##' @return A data frame with columns \code{Rank} and \code{Class} with ranks of dissimilarity.
 ##'
 ##' @export
 ##'
@@ -12,20 +12,17 @@
 ##' @author Didzis Elferts
 ##'
 ##' @examples
-##' data(dune)
-##' data(dune.env)
+##' data(dune, dune.env)
 ##' dune.dist <- vegdist(dune)
 ##' dune.ano <- with(dune.env, anosim(dune.dist, Management))
-##' 
+##'
 ##' df <- fortify(dune.ano)
-##' 
+##'
 ##' ggplot(df, aes(x = Class, y = Rank)) +
-##'       geom_boxplot(notch = TRUE, varwidth = TRUE) + 
-##'       theme(axis.title = element_blank())
-##'       
+##'     geom_boxplot(notch = FALSE, varwidth = TRUE)
+##'
 `fortify.anosim` <- function(model, data, ...) {
-  
-  df <- data.frame(Rank = model$dis.rank,
-                   Class = model$class.vec)
-  df
+    df <- data.frame(Rank = model[["dis.rank"]],
+                     Class = model[["class.vec"]])
+    df
 }
