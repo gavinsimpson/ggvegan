9e2f31c9786e6763d64fa266cf31cc1bd80e50e4
Gavin L. Simpson <ucfagls@gmail.com>
2017-12-04 11:48:56 -0600

    streamline the caption: provide a default caption rather than text in the plot area; clean-up.

--- R/autoplot.anosim.R
+++ R/autoplot.anosim.R
@@ -5,20 +5,19 @@
 ##'
 ##' @param object an object of class \code{"anosim"}, the result of a call to \code{\link[vegan]{anosim}}.
 ##' @param notch logical; make notched (default) or standard box plot?
-##' @param varwidth logical; make box width proportional to to the square-roots of the number of observations in the groups (default) or standart box plot?
+##' @param varwidth logical; make box width proportional to the square-root of the number of observations in the group (default)?
 ##' @param xlab character; label for the x-axis.
 ##' @param ylab character; label for the y-axis.
 ##' @param title character; title for the plot
 ##' @param subtitle character; subtitle for the plot
 ##' @param caption character; caption for the plot
-##' @param show.stats logical; should R and p values be added to the plot.
 ##' @param ... additional arguments passed to other methods.
 ##' @return A ggplot object.
-##' @author Didzis Elferts
+##' @author Didzis Elferts. Modifications by Gavin L. Simpson.
 ##'
 ##' @export
 ##'
-##' @importFrom ggplot2 ggplot autoplot geom_boxplot aes_string labs fortify annotate
+##' @importFrom ggplot2 ggplot autoplot geom_boxplot aes_string labs fortify
 ##'
 ##' @examples
 ##'
@@ -27,29 +26,29 @@
 ##' dune.dist <- vegdist(dune)
 ##' dune.ano <- with(dune.env, anosim(dune.dist, Management))
 ##'
-##' autoplot(dune.ano)
+##' autoplot(dune.ano, notch = FALSE)
 
-`autoplot.anosim` <- function(object, notch = TRUE, varwidth = TRUE, show.stats = TRUE,
-                              xlab = NULL, ylab = NULL, title = "Results of analysis of similarities",
-                              caption = NULL, subtitle = NULL, ...) {
-  df <- fortify(object)
-  
-  plt <- ggplot(df, aes_string(x = "Class", y = "Rank")) +
-    geom_boxplot(notch = notch, varwidth = varwidth) + 
-    labs(x = xlab, y = ylab, title = title, caption = caption)
-  
-  if(isTRUE(show.stats)) {
-    if (object$permutations) {
-      pval <- format.pval(object$signif)
+`autoplot.anosim` <- function(object, notch = TRUE, varwidth = TRUE,
+                              xlab = NULL, ylab = NULL,
+                              title = "Analysis of similarities",
+                              subtitle = NULL,
+                              caption = bquote(R == .(R) * "," ~ P == .(P)),
+                              ...) {
+    df <- fortify(object)
+
+    plt <- ggplot(df, aes_string(x = "Class", y = "Rank")) +
+        geom_boxplot(notch = notch, varwidth = varwidth)
+
+    ## prepare stats for caption --- may not be used
+    P <- if (is.null(object$permutations)) {
+        "not assessed"
+    } else {
+        format.pval(object$signif)
     }
-    else {
-      pval <- "not assessed"
-    }
-    rval <- round(object$statistic,3)
-    plt <- plt + annotate("text", x = 0.5, y = max(df$Rank, na.rm = TRUE) *1.05, hjust = 0, vjust = 0,
-                          label = paste("R = ", rval, ", ", "P = ", pval))
-  }
-  plt
+    R <- round(object$statistic,3)
+
+    ## add on labels here, not earlier due to lazy evaluation of caption default
+    plt <- plt + labs(x = xlab, y = ylab, title = title, subtitle = subtitle,
+                      caption = caption)
+    plt
 }
-
-
