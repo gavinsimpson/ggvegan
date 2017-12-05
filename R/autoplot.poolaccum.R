4a79437ee4fc4633dabe3783932e42e6eaf488b5
Gavin Simpson <ucfagls@gmail.com>
2017-12-04 22:02:42 -0600

    rewrite for style & features; prefer not to hard code colours; allow control of facetting; use geom_ribbon; documentation tweaks

--- R/autoplot.poolaccum.R
+++ R/autoplot.poolaccum.R
@@ -4,48 +4,68 @@
 ##' Produces a multi-layer ggplot object representing the output of objects produced by \code{\link[vegan]{poolaccum}}.
 ##'
 ##' @param object an object of class \code{"poolaccum"}, the result of a call to \code{\link[vegan]{poolaccum}}.
-##' @param alpha numeric; level of quantiles for envelopes shown (default 0.05).
-##' @param col character; colors of mean Richness line and qunatiles envelope.
-##' @param nrow numeric; number of rows in faceted plot.
+##' @param facet logical; should extrapolated richness estimators be shown in separate facets?
+##' @param ribbon logical; show the quantile-based uncertainty interval? Uses \code{\link{geom_ribbon}} for plot.
+##' @param ncol numeric; if facetting the plot, how many columns to use. Default arguuments will result in three columns.
+##' @param ribbon.alpha numeric; alpha transparency used for the uncertainty interval. Passed to the \code{alpha} aesthetic of \code{\link[ggplot2]{geom_ribbon}}.
 ##' @param xlab character; label for the x axis.
 ##' @param ylab character; label for the y axis.
 ##' @param title character; title for the plot.
 ##' @param subtitle character; subtitle for the plot.
 ##' @param caption character; caption for the plot.
-##' @param ... additional arguments passed to other methods.
+##' @param ... additional arguments passed to \code{\link{fortify.poolaccum}}.
+##'
 ##' @return A ggplot object.
-##' @author Didzis Elferts
+##'
+##' @author Didzis Elferts & Gavin L. Simpson
 ##'
 ##' @export
 ##'
-##' @importFrom ggplot2 ggplot autoplot geom_line aes_string labs fortify facet_wrap
-##' @importFrom stats as.formula
+##' @importFrom ggplot2 ggplot autoplot geom_line geom_ribbon aes_string labs fortify facet_wrap
 ##'
 ##' @examples
-##'
 ##' data(BCI)
 ##' pool <- poolaccum(BCI)
 ##'
 ##' autoplot(pool)
-
-`autoplot.poolaccum` <- function(object, alpha = 0.05, col = c("red","blue"), nrow = 2,
-                                 xlab = "Size", ylab = "Richness",
-                                 title = "Accumulated species richness in a species pool",
-                                 subtitle = NULL, caption = NULL,
+##'
+##' ## Turn off facetting; turns off ribbon too
+##' autoplot(pool, facet = FALSE)
+`autoplot.poolaccum` <- function(object,
+                                 facet = TRUE,
+                                 ribbon = facet,
+                                 ncol = NULL,
+                                 ribbon.alpha = 0.3,
+                                 xlab = "Size",
+                                 ylab = "Richness",
+                                 title = "Accumulated species richness",
+                                 subtitle = NULL,
+                                 caption = NULL,
                                  ...) {
-  if(length(col) != 2) {
-    stop("Exactly two colors should be provided")
-  } 
-  
-  df <- fortify(object, alpha = alpha)
-  
-  plt <- ggplot(df, aes_string(x = "Size")) +
-    geom_line(aes_string(y = "Richness"), color = col[1]) +
-    geom_line(aes_string(y = "Lower"), color = col[2]) +
-    geom_line(aes_string(y = "Upper"), color = col[2]) +
-    facet_wrap(as.formula("~ Index"), nrow = nrow) +
-    labs(x = xlab, y = ylab, title = title, subtitle = subtitle, caption = caption)
-  plt
+    ## Fortify object; pass ... here
+    df <- fortify(object, ...)
+
+    ## base plot
+    plt <- ggplot(df, aes_string(x = "Size", y = "Richness", colour = "Index"))
+
+    if (isTRUE(ribbon)) {
+        plt <- plt +
+            geom_ribbon(aes_string(ymin = "Lower", ymax = "Upper", x = "Size",
+                                   fill = "Index"),
+                        alpha = ribbon.alpha, inherit.aes = FALSE)
+    }
+
+    plt <- plt + geom_line() +
+        labs(x = xlab, y = ylab, title = title, subtitle = subtitle,
+             caption = caption)
+
+    ## are we facetting
+    if (isTRUE(facet)) {
+        if (is.null(ncol)) {
+            ncol <- (nlevels(df[["Index"]]) + 1) %/% 2
+        }
+        plt <- plt + facet_wrap("Index", ncol = ncol)
+    }
+
+    plt
 }
-
-
