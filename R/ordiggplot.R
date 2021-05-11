gavinsimpson/ggvegan

commit 5228998e2133595673454648077c68c21c140a3e

allow separate modification of arrow and text parameters

R/ordiggplot.R
@@ -161,27 +161,41 @@
         data <- ~.x[.x$Score == Score,]
     geom_label(data = data, ...)
 }
+
+#' @param arrow.params,text.params Parameters to modify arrows or
+#'     their text labels.
+#'
 #' @importFrom ggplot2 geom_segment geom_label geom_text aes
 #' @importFrom grid arrow
+#' @importFrom utils modifyList
+#'
 #' @rdname ordiggplot
 #' @export
 `geom_ordiarrow` <-
-    function(Score, data, text = TRUE, box = FALSE, ...)
+    function(Score, data, text = TRUE, box = FALSE,
+             arrow.params=list(), text.params=list(), ...)
 {
     if (missing(Score) && missing(data))
         stop("either Score or data must be defined")
     if (missing(data))
         data <- ggscores(Score)
-    pl <- geom_segment(data = data, mapping = aes(xend = 0, yend = 0),
-                       arrow = arrow(ends = "first", length=unit(0.2, "cm")),
-                       ...)
+    ## default params & possible modification
+    arrowdefs <- list(arrow = arrow(ends = "first", length=unit(0.2, "cm")))
+    textdefs <- list(vjust = "outward", hjust = "outward")
+    arrowdefs <- modifyList(arrowdefs, arrow.params)
+    textdefs <- modifyList(textdefs, text.params)
+    ## graphics
+    pl <- do.call("geom_segment",
+                  modifyList(list(data = data,
+                                  mapping = aes(xend = 0, yend = 0)),
+                                  arrowdefs))
     if (text) {
         if(box)
-            p2 <- geom_label(data = data, vjust = "outward", hjust = "outward",
-                             ...)
+            p2 <- do.call("geom_label",
+                          modifyList(list(data = data), textdefs))
         else
-            p2 <- geom_text(data = data, vjust = "outward", hjust = "outward",
-                            ...)
+            p2 <- do.call("geom_text",
+                          modifyList(list(data = data), textdefs))
         pl <- list(pl, p2) ## ggprotos cannot be added (+)
     }
     pl
@@ -286,7 +300,7 @@
 #'     multiplier is selected automatically so that arrows fit the
 #'     current graph.
 #' @param ... Other arguments passed to the functions.
-#' 
+#'
 #' @examples
 #' data(mite, mite.env)
 #' m <- metaMDS(mite, trace=FALSE, trymax=100)

NAMESPACE
@@ -65,5 +65,6 @@ importFrom(stats,model.frame)
 importFrom(stats,setNames)
 importFrom(stats,weights)
 importFrom(tidyr,gather)
+importFrom(utils,modifyList)
 importFrom(vegan,scores)
 importFrom(vegan,vectorfit)

man/ordiggplot.Rd
@@ -18,7 +18,15 @@ geom_orditext(Score, data, ...)
 
 geom_ordilabel(Score, data, ...)
 
-geom_ordiarrow(Score, data, text = TRUE, box = FALSE, ...)
+geom_ordiarrow(
+  Score,
+  data,
+  text = TRUE,
+  box = FALSE,
+  arrow.params = list(),
+  text.params = list(),
+  ...
+)
 
 geom_ordiaxis(lty = 3, ...)
 
@@ -44,6 +52,9 @@ instead of \code{Score}.}
 \item{text}{Add text labels to the plot.}
 
 \item{box}{Draw a box behind the text (logical).}
+
+\item{arrow.params, text.params}{Parameters to modify arrows or
+their text labels.}
 
 \item{lty}{Linetype.}
 }

commit 8281fe9a9c130c33ac8760081dbbe26f5f38bcae

plot envfit arrows was broken in the previous commit 5228998e21

R/ordiggplot.R
@@ -184,6 +184,11 @@
     textdefs <- list(vjust = "outward", hjust = "outward")
     arrowdefs <- modifyList(arrowdefs, arrow.params)
     textdefs <- modifyList(textdefs, text.params)
+    dots <- match.call(expand.dots = FALSE)$...
+    if (!is.null(dots)) {
+        arrowdefs <- modifyList(arrowdefs, dots)
+        textdefs <- modifyList(textdefs, dots)
+    }
     ## graphics
     pl <- do.call("geom_segment",
                   modifyList(list(data = data,