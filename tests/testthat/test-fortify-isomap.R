ca8f271f56678868a90a761dc8f4d5f9217d08df
Gavin Simpson <ucfagls@gmail.com>
2026-02-14 21:03:19 +0100

    reorder tests

--- tests/testthat/test-fortify-isomap.R
+++ tests/testthat/test-fortify-isomap.R
@@ -1,17 +1,19 @@
 # Tests for fortify.isomap()
 
 test_that("fortify works for isomap objects", {
+  expect_s3_class(fortify(dune_isomap), "tbl_df")
+  expect_s3_class(fortify(dune_isomap, what = "network"), "tbl_df")
+
   skip_on_ci()
   expect_snapshot(fortify(dune_isomap))
   expect_snapshot(fortify(dune_isomap, what = "network"))
-  expect_s3_class(fortify(dune_isomap), "tbl_df")
-  expect_s3_class(fortify(dune_isomap, what = "network"), "tbl_df")
 })
 
 test_that("tidy works for isomap objects", {
+  expect_s3_class(tidy(dune_isomap), "tbl_df")
+  expect_s3_class(tidy(dune_isomap, what = "network"), "tbl_df")
+
   skip_on_ci()
   expect_snapshot(tidy(dune_isomap))
   expect_snapshot(tidy(dune_isomap, what = "network"))
-  expect_s3_class(tidy(dune_isomap), "tbl_df")
-  expect_s3_class(tidy(dune_isomap, what = "network"), "tbl_df")
 })
