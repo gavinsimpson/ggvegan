d48a1cc8306c5d7d38f9891ed0c94a6aca15631a
Gavin Simpson <ucfagls@gmail.com>
2026-02-14 20:44:16 +0100

    skip a test on CI

--- tests/testthat/test-fortify-isomap.R
+++ tests/testthat/test-fortify-isomap.R
@@ -1,6 +1,7 @@
 # Tests for fortify.isomap()
 
 test_that("fortify works for isomap objects", {
+  skip_on_ci()
   expect_snapshot(fortify(dune_isomap))
   expect_snapshot(fortify(dune_isomap, what = "network"))
   expect_s3_class(fortify(dune_isomap), "tbl_df")
@@ -8,6 +9,7 @@ test_that("fortify works for isomap objects", {
 })
 
 test_that("tidy works for isomap objects", {
+  skip_on_ci()
   expect_snapshot(tidy(dune_isomap))
   expect_snapshot(tidy(dune_isomap, what = "network"))
   expect_s3_class(tidy(dune_isomap), "tbl_df")
