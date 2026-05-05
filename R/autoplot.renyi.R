50625550b7182f2c4366ee8d5902d53b00a5132d
Jari Oksanen <jhoksane@gmail.com>
2026-05-05 13:34:07 +0300

    update renyi functions to the ggvegan style

--- R/autoplot.renyi.R
+++ R/autoplot.renyi.R
@@ -1,36 +1,27 @@
 ### autoplot & fortify functions for
 
-#' Autoplot Graphics for vegan renyi and renyiaccum Objects
+#' Autoplot Graphics for vegan Rényi and Tsallis diversity Objects
 #'
-#' Alternatives to \pkg{lattice} graphics functions in
-#' \pkg{vegan}.
-#'
-#' \code{autoplot} for \code{\link[vegan]{renyi}} displays the
-#' diversites against alpha as dots over a ribbon of data extremes in
-#' the graph and line of data median faceted by sites. \code{autoplot}
-#' for \code{\link[vegan]{renyiaccum}} shows the median of diversity
-#' accumulation against the number of sites over a ribbon of
-#' permutation values faceted by alpha.
+#' \code{autoplot} for [vegan::renyi()] and [vegan::tsallis()] plots
+#' the diversites against diversity salce as dots over a ribbon of
+#' data extremes in the graph and line of data median, faceted by
+#' sites.
 #'
 #' @examples
 #' ## Rényi
+#' library(vegan)
 #' data(BCI)
 #' mod <- renyi(BCI[sample(50, 16),])
 #' autoplot(mod)
-#' ## Diversity against the number of sites
-#' mod <- renyiaccum(BCI)
-#' autoplot(mod)
-#' autoplot(mod) + scale_x_log10()
 #'
-#' @param object Result from \pkg{vegan} \code{\link[vegan]{renyi}} or
-#'     \code{\link[vegan]{renyiaccum}} functions.
+#' @param object Result from [vegan::renyi()] or [vegan::tsallis()]
+#'     functions.
 #'
 #' @param point.params,line.params,ribbon.params List of graphical
-#'     parameters passed to \code{\link[ggplot2]{geom_point}},
-#'     \code{\link[ggplot2]{geom_line}},
-#'     \code{\link[ggplot2]{geom_ribbon}}.
+#'     parameters passed to [ggplot2::geom_point()],
+#'     [ggplot2::geom_line()] or [ggplot2::geom_ribbon()].
 #'
-#' @importFrom ggplot2 fortify ggplot aes_ geom_point geom_ribbon geom_line
+#' @importFrom ggplot2 fortify ggplot aes geom_point geom_ribbon geom_line
 #'      facet_wrap
 #' @importFrom utils modifyList
 
@@ -40,22 +31,24 @@
              ribbon.params = list(),  ...)
 {
     df <- fortify(object)
-    ## geom ordering is weird because alpha is a factor, but
+    ## geom ordering is weird because scale is a factor, but
     ## geom_ribbon and geom_line need continuous x and geom_point
-    ## against factor alpha must be befor them
+    ## against factor alpha must be before them
     ribbon.params = modifyList(
-        list(mapping = aes_(x = ~as.numeric(alpha), ymin = ~lo, ymax = ~hi),
+        list(mapping = aes(x = as.numeric(.data[["scale"]]),
+                           ymin = .data[["lo"]], ymax = .data[["hi"]]),
              fill = "skyblue", alpha = 0.2),
         ribbon.params)
     line.params = modifyList(
-        list(mapping = aes_(x = ~as.numeric(alpha), y = ~median),
+        list(mapping = aes(x = as.numeric(.data[["scale"]]),
+                           y = .data[["median"]]),
              colour = "blue"),
         line.params)
-    ggplot(df, aes_(~alpha, ~Diversity)) +
+    ggplot(df, aes(.data[["scale"]], .data[["diversity"]])) +
         do.call("geom_point", point.params) +
         do.call("geom_ribbon", ribbon.params) +
         do.call("geom_line", line.params) +
-        facet_wrap(~Site)
+        facet_wrap(~site)
 }
 
 #' @inheritParams ggplot2::fortify
@@ -72,10 +65,10 @@
     } else {
         model <- as.matrix(model)
         df <- data.frame(
-            "Diversity" = as.vector(model),
-            "alpha" = factor(rep(colnames(model), each = nrow(model)),
+            "diversity" = as.vector(model),
+            "scale" = factor(rep(colnames(model), each = nrow(model)),
                                  levels = colnames(model)),
-            "Site" = factor(rep(rownames(model), ncol(model)),
+            "site" = factor(rep(rownames(model), ncol(model)),
                             levels = rownames(model)),
             "median" = rep(apply(model, 2, median), each=nrow(model)),
             "lo" = rep(apply(model, 2, min), each=nrow(model)),
