a6d7e720dceb43118ed8cd110e9b8ac190769518
Jari Oksanen <jhoksane@gmail.com>
2026-05-06 08:59:09 +0300

    split fortify.renyi, add tidy, polish and check

--- R/autoplot.renyi.R
+++ R/autoplot.renyi.R
@@ -1,6 +1,7 @@
 ### autoplot & fortify functions for
 
-#' Autoplot Graphics for vegan Rényi and Tsallis diversity Objects
+#' Autoplot Graphics for vegan `renyi()` and `tsallis()` diversity
+#' Objects
 #'
 #' \code{autoplot} for [vegan::renyi()] and [vegan::tsallis()] plots
 #' the diversites against diversity salce as dots over a ribbon of
@@ -11,24 +12,23 @@
 #' ## Rényi
 #' library(vegan)
 #' data(BCI)
-#' mod <- renyi(BCI[sample(50, 16),])
+#' mod <- renyi(BCI[1:16,])
 #' autoplot(mod)
 #'
-#' @param object Result from [vegan::renyi()] or [vegan::tsallis()]
-#'     functions.
-#'
+#' @inheritParams ggplot2::autoplot
+#' #'
 #' @param point.params,line.params,ribbon.params List of graphical
 #'     parameters passed to [ggplot2::geom_point()],
 #'     [ggplot2::geom_line()] or [ggplot2::geom_ribbon()].
 #'
-#' @importFrom ggplot2 fortify ggplot aes geom_point geom_ribbon geom_line
-#'      facet_wrap
+#' @importFrom ggplot2 fortify ggplot aes geom_point geom_ribbon
+#'     geom_line facet_wrap
 #' @importFrom utils modifyList
 
 #' @export
 `autoplot.renyi` <-
     function(object, point.params = list(), line.params = list(),
-             ribbon.params = list(),  ...)
+             ribbon.params = list(), ...)
 {
     df <- fortify(object)
     ## geom ordering is weird because scale is a factor, but
@@ -49,30 +49,4 @@
         do.call("geom_ribbon", ribbon.params) +
         do.call("geom_line", line.params) +
         facet_wrap(~site)
-}
-
-#' @inheritParams ggplot2::fortify
-
-#' @importFrom stats median
-#'
-#' @rdname autoplot.renyi
-#' @export
-`fortify.renyi` <-
-    function(model, data, ...)
-{
-    if (!is.data.frame(model)) {
-        stop("not yet implemented for single sites")
-    } else {
-        model <- as.matrix(model)
-        df <- data.frame(
-            "diversity" = as.vector(model),
-            "scale" = factor(rep(colnames(model), each = nrow(model)),
-                                 levels = colnames(model)),
-            "site" = factor(rep(rownames(model), ncol(model)),
-                            levels = rownames(model)),
-            "median" = rep(apply(model, 2, median), each=nrow(model)),
-            "lo" = rep(apply(model, 2, min), each=nrow(model)),
-            "hi" = rep(apply(model, 2, max), each=nrow(model)))
-    }
-    df
 }
---
+++ R/fortify.renyi.R
@@ -0,0 +1,40 @@
+#' @inheritParams ggplot2::fortify
+#' @inheritParams generics::tidy
+#'
+#' @title Fortify vegan `renyi()` or `tsallis()` diversity results
+#'
+#' @description Prepares a fortified versionof results from
+#'     [vegan::renyi()] and [vegan::tsallis()].
+#'
+#' @importFrom stats median
+#'
+#' @rdname fortify.renyi
+#' @export
+`fortify.renyi` <-
+    function(model, data, ...)
+{
+    if (!is.data.frame(model)) {
+        stop("not yet implemented for single sites")
+    } else {
+        model <- as.matrix(model)
+        df <- data.frame(
+            "diversity" = as.vector(model),
+            "scale" = factor(rep(colnames(model), each = nrow(model)),
+                                 levels = colnames(model)),
+            "site" = factor(rep(rownames(model), ncol(model)),
+                            levels = rownames(model)),
+            "median" = rep(apply(model, 2, median), each=nrow(model)),
+            "lo" = rep(apply(model, 2, min), each=nrow(model)),
+            "hi" = rep(apply(model, 2, max), each=nrow(model)))
+    }
+    df
+}
+
+#' @importFrom tibble as_tibble
+#' @rdname fortify.renyi
+#' @export
+`tidy.renyi` <-
+    function(x, data, ...)
+{
+    as_tibble(fortify(model = x, data = data, ...))
+}
